2828e652451b6f3f082ecf65deedf17f84f4a2f9
umtdemr <umitde692@gmail.com>
2022-06-29 20:44:34 +0300

    created tab header

---
+++ components/form/Form.tsx
@@ -0,0 +1,29 @@
+import react from 'react'
+import styles from './Form.module.scss'
+
+
+const Form: react.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.slogan}>
+        the<br/>shorter,<br/> the<br/> better
+      </h2>
+      <div className={styles.tab}>
+        <div className={styles.tab__header}>
+          <div className={styles.spans}>
+            <span className={styles.close} />
+            <span className={styles.resize} />
+            <span className={styles.min} />
+          </div>
+          <h3 className={styles.tab_title}>shortql</h3>
+        </div>
+        <div className={styles.tab__content}>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Form
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
+import Form from '../components/form/Form'
 import Header from '../components/header/Header'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <div className="wrapper">
+          <Form />
+        </div>
+      </main>
+    </>
   )
 }
 
