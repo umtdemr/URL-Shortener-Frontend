a18a23be19240cc66e067d38c1cc2595feea3a46
umtdemr <umitde692@gmail.com>
2022-07-01 17:35:10 +0300

    added apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { ApolloClient, ApolloProvider, gql, InMemoryCache } from '@apollo/client'
+
+
+// TODO: make sure url will be provided from env file in future 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
