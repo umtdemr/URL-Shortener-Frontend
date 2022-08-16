3adef50dd6f70b01a4f8ea52fbe4ceb29d909f1b
umtdemr <umitde692@gmail.com>
2022-08-16 22:44:48 +0300

    added head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Form from '../components/form/Form'
 import Header from '../components/header/Header'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>ShortQL</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <main>
         <div className="wrapper">
